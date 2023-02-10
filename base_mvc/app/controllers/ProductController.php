f193c2988e7da459779db49902b930e31d4e1d02
thuytx03 <111112338+thuytx03@users.noreply.github.com>
2023-02-10 21:50:50 +0700

    base_mvc

---
+++ base_mvc/app/controllers/ProductController.php
@@ -0,0 +1,14 @@
+<?php
+    namespace App\Controllers;
+    class ProductController {
+        // public static function index(){
+        //     return "Đây là trang chủ";
+        // }
+        public function index(){
+            echo "123";
+        }
+        public function addProduct(){
+            echo "Thêm sản phẩm";
+        }
+    }
+?>
---
+++ base_mvc/env.php
@@ -0,0 +1,8 @@
+<?php
+//điều chỉnh kết nối db ở đây
+const DBNAME = "we17316";
+const DBUSER = "root";
+const DBPASS = "";
+const DBCHARSET = "utf8";
+const DBHOST = "127.0.0.1";
+
